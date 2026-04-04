23669e06118b7b8550b39be731560c64f75c0422
rkochp <rkochp@ext.uber.com>
2026-04-04 10:39:14 -0300

    [be] Add a basic hello endpoint for testing

--- backend/main.go
+++ backend/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := flag.String("p", "8080", "port to listen on")
+	flag.Parse()
+
+	http.DefaultServeMux.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"message":"hello"}`))
+	})
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
+}
